b3376f0cee69191a27f899669420aef656507588
Jia <49358172+craftslab@users.noreply.github.com>
2022-03-15 13:07:12 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
+        "eslint:recommended"
     ],
     "parserOptions": {
         "ecmaVersion": 13,
